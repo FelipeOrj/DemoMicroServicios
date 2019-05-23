960ce13dc8a2864822f89900714c9bad5ca2d884
FelipeOrj <felipeorj@gmail.com>
2019-05-23 15:59:53 -0500

    Update CardStatementCompositeApplication.java

--- card-statement-composite/src/main/java/com/springnetflixoss/microservices/cardstatementcomposite/CardStatementCompositeApplication.java
+++ card-statement-composite/src/main/java/com/springnetflixoss/microservices/cardstatementcomposite/CardStatementCompositeApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@ComponentScan(basePackages = {"com.springnetflixoss.microservices"})
 public class CardStatementCompositeApplication {
 
 	public static void main(String[] args) {
